1b6ac948330b440385ac5f78b31931af9c126ecd
unknown <karenwli@seas.upenn.edu>
2022-02-26 00:26:59 -0800

    'first

--- ethereum_query.py
+++ ethereum_query.py
@@ -1,20 +1,22 @@
 from web3 import Web3
 from hexbytes import HexBytes
 
-IP_ADDR='18.188.235.196'
-PORT='8545'
+IP_ADDR = '18.188.235.196'
+PORT = '8545'
 
 w3 = Web3(Web3.HTTPProvider('http://' + IP_ADDR + ':' + PORT))
 
 if w3.isConnected():
-#     This line will mess with our autograders, but might be useful when debugging
-    print( "Connected to Ethereum node" )
+    #     This line will mess with our autograders, but might be useful when debugging
+    print("Connected to Ethereum node")
 else:
-    print( "Failed to connect to Ethereum node!" )
+    print("Failed to connect to Ethereum node!")
+
 
 def get_transaction(tx):
-    transaction = w3.get_transaction(tx)   #YOUR CODE HERE
+    transaction = w3.get_transaction(tx)  # YOUR CODE HERE
     return transaction
+
 
 # Return the gas price used by a particular transaction,
 #   tx is the transaction
@@ -23,19 +25,23 @@ def get_gas_price(tx):
     gas_price = transaction['gasPrice']
     return gas_price
 
+
 def get_gas(tx):
-    gas = 1 #YOUR CODE HERE
+    gas = 1  # YOUR CODE HERE
     return gas
 
+
 def get_transaction_cost(tx):
-    tx_cost = 1 #YOUR CODE HERE
+    tx_cost = 1  # YOUR CODE HERE
     return tx_cost
 
+
 def get_block_cost(block_num):
-    block_cost = 1  #YOUR CODE HERE
+    block_cost = 1  # YOUR CODE HERE
     return block_cost
+
 
 # Return the hash of the most expensive transaction
 def get_most_expensive_transaction(block_num):
-    max_tx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  #YOUR CODE HERE
+    max_tx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  # YOUR CODE HERE
     return max_tx
